edadc961eb3a9875d8a6ee1f0a808bede4fec369
zhongkui <1248835207@qq.com>
2017-12-16 18:53:31 +0800

    js处理

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,15 @@ gulp.task('revCss', function() {                                //- 创建一个
         .pipe(rev.manifest())                                   //- 生成一个rev-manifest.json
         .pipe(gulp.dest('./rev/css'));                              //- 将 rev-manifest.json 保存到 rev 目录内
 });
+gulp.task('revJs', function() {                                //- 创建一个名为 concat 的 task
+    return gulp.src('./targetFile/ceshi.js')    //- 需要处理的js文件，放到一个字符串数组里
+        .pipe(rev())                                            //- 文件名加MD5后缀
+        .pipe(gulp.dest('./dist'))                              //- 输出文件本地
+        .pipe(rev.manifest())                                   //- 生成一个rev-manifest.json
+        .pipe(gulp.dest('./rev/js'));                              //- 将 rev-manifest.json 保存到 rev 目录内
+});
 gulp.task('revHtml', function() {
-    return gulp.src(['./rev/css/*.json', './targetFile/*.html'])   //- 读取 rev-manifest.json 文件以及需要进行css名替换的文件
+    return gulp.src(['./rev/*/*.json', './targetFile/*.html'])   //- 读取 rev-manifest.json 文件以及需要进行css名替换的文件
         .pipe(revCollector())                                   //- 执行文件内css名的替换
         .pipe(gulp.dest('./dist/'));                     //- 替换后的文件输出的目录
 });
@@ -28,6 +35,7 @@ gulp.task('md5', function (done) {
     runSequence(
         ['clean'],
         ['revCss'],
+        ['revJs'],
         ['revHtml'],
         done
     );
