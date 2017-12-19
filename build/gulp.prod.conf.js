8d8244db3a47bbd5ff8f513e34d6c9ca94892e5d
talkmate <1248835207@qq.com>
2017-12-19 14:27:03 +0800

    css js 压缩添加

--- build/gulp.prod.conf.js
+++ build/gulp.prod.conf.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');         // css 压缩
+var uglify = require('gulp-uglify');              // js压缩
 var runSequence = require('run-sequence');
 var del = require('del')
 var rev = require('gulp-rev');                                  //- 对文件名加MD5后缀
 var revCollector = require('gulp-rev-collector');               //- 路径替换
-function dev () {
+function build () {
     gulp.task('revCss', function() {                                //- 创建一个名为 concat 的 task
         return gulp.src('./targetFile/ceshi.css')    //- 需要处理的css文件，放到一个字符串数组里
             .pipe(rev())                                            //- 文件名加MD5后缀
+            .pipe(cleanCSS())                                       // css 压缩
             .pipe(gulp.dest('./dist'))                              //- 输出文件本地
             .pipe(rev.manifest())                                   //- 生成一个rev-manifest.json
             .pipe(gulp.dest('./rev/css'));                             //- 将 rev-manifest.json 保存到 rev 目录内
@@ -14,6 +17,7 @@ function dev () {
     gulp.task('revJs', function() {                                //- 创建一个名为 concat 的 task
         return gulp.src('./targetFile/ceshi.js')    //- 需要处理的js文件，放到一个字符串数组里
             .pipe(rev())                                            //- 文件名加MD5后缀
+            .pipe(uglify())                                         // js 压缩
             .pipe(gulp.dest('./dist'))                              //- 输出文件本地
             .pipe(rev.manifest())                                   //- 生成一个rev-manifest.json
             .pipe(gulp.dest('./rev/js'));                             //- 将 rev-manifest.json 保存到 rev 目录内
