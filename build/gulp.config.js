a090e8fd642f813effd7f9b74dd3987ef204c3e4
talkmate <1248835207@qq.com>
2018-01-02 15:40:14 +0800

    配置更改

---
+++ build/gulp.config.js
@@ -0,0 +1,9 @@
+var cdnUrl = 'http://beat-join.talkmate.com'
+var configPath = {
+  cdnUrlCss: cdnUrl + '/static/css',
+  cdnUrlJs: cdnUrl + '/static/js',
+  cdnUrlImg: cdnUrl + '/static/img',
+  cdnUrlPlugin: cdnUrl + '/static/plugin',
+  targetHtm: './targetFile/htm/**' // 目标模板路径
+}
+module.exports = configPath
